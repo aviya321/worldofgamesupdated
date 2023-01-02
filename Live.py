073ada7fe17129fc2a08ce6910f9215e88de5668
aviya321 <116565022+aviya321@users.noreply.github.com>
2023-01-02 12:41:21 +0200

    Update Live.py
    changed import commands to top of page

--- Live.py
+++ Live.py
@@ -1,3 +1,8 @@
+import GuessGame
+import MemoryGame
+import CurrencyRouletteGame
+
+
 # The function has a person name and returns a string
 def welcome_name():
     name = input("Please enter your name: ")
@@ -7,9 +12,9 @@ welcome_name()
 
 # The function prints out the following text and make sure numbers are between 1-3, 1-5
 def load_game():
-    print("1.Memory Game - a sequence of numbers will appear for 1 second and you have to guess it back \n2.Guess Game - guess a number and see if you chose like the computer\n3.Currency Roulette - try and guess the value of a random amount of USD in ILS")
+    print('''1.Memory Game - a sequence of numbers will appear for 1 second and you have to guess it back. 2.Guess Game - guess a number and see if you chose like the computer. 3.Currency Roulette - try and guess the value of a random amount of USD in ILS.''')
 
-    game_chosen = (input('Please choose a game to select from 1 to 3:'))
+    game_chosen = input('Please choose a game to select from 1 to 3:')
 
     while not 3 >= int(game_chosen) >= 1:
         game_chosen = input('its not a valid number, please try again')
@@ -25,17 +30,17 @@ def load_game():
     print('Thank you, you have entered level of difficulty:',difficulty_chosen)
 
 
+
+    if game_chosen == 1:
+        GuessGame.play1()
+    elif game_chosen == 2:
+        MemoryGame.play2()
+    elif game_chosen == 3:
+        CurrencyRouletteGame.play3()
+
+
+
 load_game()
 
-import GuessGame
-import MemoryGame
-import CurrencyRouletteGame
-
-if game_chosen == 1:
-    GuessGame.play1()
-elif game_chosen == 2:
-    MemoryGame.play2()
-elif game_chosen == 3:
-    CurrencyRouletteGame.play3()
 
 
