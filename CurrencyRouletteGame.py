0af699e8dbe0ae64e9b8e93122b06522b720f68f
aviya321 <116565022+aviya321@users.noreply.github.com>
2023-01-08 21:35:49 +0200

    Update CurrencyRouletteGame.py

--- CurrencyRouletteGame.py
+++ CurrencyRouletteGame.py
@@ -1,49 +1,32 @@
 
 import random
 from currency_converter import CurrencyConverter
-print("Hello and welcome to Currency Roulette Game!")
 
-
-def get_secret_num():
-    global secret_number
+def get_money_interval(difficulty):
+    c = CurrencyConverter()
+    crncy = c.convert(1, 'USD', 'ILS')
+    f_crncy = float(crncy)
     secret_number = random.randint(1, 100)
     print("Your random number is", secret_number, "dollars")
+    print(f"What do you think will be the {secret_number} in ILS? ")
+    suggested_guess = get_guess_from_user()
+    real_number = secret_number * f_crncy
+    if (real_number - (5 - difficulty)) < suggested_guess < (real_number + (5 - difficulty)):
+        print("You are very close! You win!")
+        return True
+    else:
+        print("You are very far! You lose!")
+        return False
+
+
+
 
 
 def get_guess_from_user():
-    difficulty = input("Please enter again your level of difficulty:")
-    while not difficulty.isdigit():
-        difficulty = int(input('its not a valid number, please try again'))
-    print('Thank you, you have entered difficulty:', difficulty)
-    c = CurrencyConverter()
-    crncy = c.convert(1, 'USD', 'ILS')
-    user_guess = int(input("Please enter your suggested value of dollars above in ILS: "))
-    guess = round(user_guess / crncy)
-    print(f"Your exchange rate is {guess} dollars")
-    global diff_sec_guess
-    diff_sec_guess = int(secret_number - guess)
-    global abs_diff_sec_guess
-    abs_diff_sec_guess = abs(diff_sec_guess)
-    print(f"You are {abs_diff_sec_guess} shekels far from the real number")
-    if int(abs_diff_sec_guess) < int(difficulty):
-        print("You are very close! You win!")
-    else:
-        print("You are very far! You lose!")
-
-def play_again():
-    global again
-    again = input("Do you want to try again? Y/N?")
-    if again == "Y" or again == "y":
-        return play()
-
-    else:
-        print("Thanks for your time. Hope you enjoyed!")
+    user_guess = int(input("Please guess the value of the amount:"))
+    return user_guess
 
 
-def play3():
-    get_secret_num()
-    get_guess_from_user()
-    play_again()
 
-play3()
-
+def play3(difficulty):
+    get_money_interval(difficulty)
