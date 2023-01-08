0eaeb2906957fb17b143e64fc0443ad0f9bb3224
aviya321 <116565022+aviya321@users.noreply.github.com>
2023-01-08 21:34:50 +0200

    Update MainGame.py

--- MainGame.py
+++ MainGame.py
@@ -1,9 +1,16 @@
-from Live import load_game, welcome_name
+from Live import load_game, welcome
 
-welcome_name()
+name = input('What is your name?')
+welcome(name)
 load_game()
 
 
+def play_again():
+    again = input("Do you want to try again? Y/N?")
+    if again == "Y" or again == "y":
+        return play()
+    else:
+        print("Thanks for your time. Hope you enjoyed!")
 
 
 
