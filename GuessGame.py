c2ec7de342120f432b90772d4defa57c1114fd4f
aviya321 <116565022+aviya321@users.noreply.github.com>
2023-01-08 21:36:37 +0200

    Update GuessGame.py

--- GuessGame.py
+++ GuessGame.py
@@ -1,46 +1,35 @@
-import random
+from random import randrange
+
+
+def generate_number(number):
+    return randrange(number)
+
 
 def get_guess_from_user():
-    global difficulty
-    difficulty = input("Please enter again your difficulty level:")
-    while not difficulty.isdigit():
-        difficulty = input("It is not a valid number,please try again!")
-    global get_guess
-    get_guess = input("Please enter a number between 1 and your difficulty level:")
-    while not get_guess.isdigit():
-        get_guess = input("It is not a valid number,please try again!")
-    print(f"Your guess number is {get_guess}")
+    return int(input('Please guess a number:'))
 
 
-
-def generate_number():
-    global secret_number
-    secret_number = random.randint(1,int(difficulty))
-    print(f"Your secret number is {secret_number}")
-
-
-def compare_results():
-    if secret_number == get_guess:
-        print("Your guess is perfect! You win!")
+def compare_results(random_number, user_guess):
+    if random_number == user_guess:
+        print("You are right!")
     else:
-        print("You guess is wrong! You lose!")
+        print("You are wrong!")
+    return random_number == user_guess
 
-def play_again():
-    global again
-    again = input("Do you want to try again? Y/N?")
-    if again == "Y" or again == "y":
-        return play()
 
-    else:
-        print("Thanks for your time. Hope you enjoyed!")
+def play1(difficulty):
+    random_number = generate_number(difficulty)
+    user_guess = get_guess_from_user()
+    return compare_results(random_number, user_guess)
 
-def play1():
-    get_guess_from_user()
-    generate_number()
-    compare_results()
-    play_again()
 
-play1()
+
+
+
+
+
+
+
 
 
 
