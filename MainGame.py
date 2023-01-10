2718df3b8333f9683eb38b8147310b36eb80f14c
aviya321 <116565022+aviya321@users.noreply.github.com>
2023-01-10 17:43:35 +0200

    Update MainGame.py

--- MainGame.py
+++ MainGame.py
@@ -5,14 +5,6 @@ welcome(name)
 load_game()
 
 
-def play_again():
-    again = input("Do you want to try again? Y/N?")
-    if again == "Y" or again == "y":
-        return play()
-    else:
-        print("Thanks for your time. Hope you enjoyed!")
-
-
 
 
 
