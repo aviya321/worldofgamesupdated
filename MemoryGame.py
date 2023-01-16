dc24187861fdcbfb69fc8e979117cfa3152dd212
aviya321 <116565022+aviya321@users.noreply.github.com>
2023-01-16 09:20:43 +0200

    Update MemoryGame.py

--- MemoryGame.py
+++ MemoryGame.py
@@ -2,6 +2,8 @@ from random import randint
 import os
 from time import sleep
 
+from Score import add_score
+
 
 def generate_sequence(difficulty):
     random_list = []
@@ -17,15 +19,16 @@ def get_list_from_user(difficulty):
     user_list = input('Please enter the numbers you have just seen:').split(" ")
     user_list = [int(i) for i in user_list]
     return user_list
-   
 
 
 
-def is_list_equal(random_list, user_list):
+
+def is_list_equal(random_list, user_list,difficulty):
     print(random_list)
     print(user_list)
     if random_list == user_list:
         print('results are equal - you win!')
+        add_score(difficulty)
         return True
     else:
         print('results are not equal - you lose!')
@@ -35,7 +38,5 @@ def is_list_equal(random_list, user_list):
 def play2(difficulty):
     random_list = generate_sequence(difficulty)
     user_list = get_list_from_user(difficulty)
-    return is_list_equal(random_list, user_list)
-
-
+    return is_list_equal(random_list, user_list,difficulty)
 
