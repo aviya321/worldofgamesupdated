ac5b60dade2747c0ab1e3bc558f7b6e8ecd43045
aviya321 <116565022+aviya321@users.noreply.github.com>
2023-01-16 09:17:24 +0200

    Update Score.py

--- Score.py
+++ Score.py
@@ -1,24 +1,34 @@
 from Utils import *
 from os import path
 
-def calculate_score(difficulty):
-    POINTS_OF_WINNING = int((difficulty*3)+5)
-    scores_file = open(SCORES_FILE_NAME, "r")
-    current_score = int(scores_file.readline())
-    scores_file.close()
-    new_score = current_score + POINTS_OF_WINNING
-    print(new_score)
-
 
 def add_score(difficulty):
-    if path.exists(SCORES_FILE_NAME):
-        new_score = calculate_score(difficulty)
-        scores_file = open(SCORES_FILE_NAME, "w")
-        scores_file.write(str(new_score))
+    new_score = (difficulty * 3) + 5
+    try:
+        if not os.path.getsize("Scores.txt") == 0:
+            with open("Scores.txt", "r") as score_file:
+                current_score = score_file.readline()
+                print(current_score)
+                new_score += int(current_score)
+    except FileNotFoundError:
+        with open("Scores.txt", "w") as score_file:
+            score_file.write(str(new_score))
+            print(new_score)
+
     else:
-        score_file = open(SCORES_FILE_NAME, "w")
-        score_file.write("0")
-        score_file.close()
+        with open("Score.txt", "w") as score_file:
+            score_file.write(str(new_score))
+            print(new_score)
+
+
+
+
+
+
+
+
+
+
 
 
 
