57630b67902e5e0aea6497fa871de87f8ed2af30
aviya321 <116565022+aviya321@users.noreply.github.com>
2023-01-16 09:09:33 +0200

    Update GuessGame.py

--- GuessGame.py
+++ GuessGame.py
@@ -1,17 +1,19 @@
 from random import randrange
+from Score import add_score
 
 
 def generate_number(number):
-    return randrange(number)
+    return randrange(int(number))
 
 
 def get_guess_from_user():
     return int(input('Please guess a number:'))
 
 
-def compare_results(random_number, user_guess):
+def compare_results(random_number, user_guess, difficulty):
     if random_number == user_guess:
         print("You are right!")
+        add_score(difficulty)
     else:
         print("You are wrong!")
     return random_number == user_guess
@@ -20,9 +22,7 @@ def compare_results(random_number, user_guess):
 def play1(difficulty):
     random_number = generate_number(difficulty)
     user_guess = get_guess_from_user()
-    return compare_results(random_number, user_guess)
-
-
+    return compare_results(random_number, user_guess, difficulty)
 
 
 
